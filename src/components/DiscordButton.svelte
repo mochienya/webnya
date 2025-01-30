7b12d2d321da30fa932f05a90a8d342d676989ef
mochie <mop48duck+cassie@gmail.com>
2025-01-31 00:57:43 +0100

    add homepage and semi major refactor?

---
+++ src/components/DiscordButton.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+    import Icon from "@iconify/svelte";
+
+  let timeoutId: number | undefined
+  function onclick() {
+    clearTimeout(timeoutId)
+    navigator.clipboard.writeText("mochie.nya")
+    messageShown = true
+    timeoutId = setTimeout(() => {
+      messageShown = false
+    }, 1250);
+  }
+  let messageShown = $state(false)
+</script>
+
+<div>
+  {#if messageShown}
+    <span id="copied">copied :3</span>
+  {/if}
+  <button {onclick}>
+    <Icon icon="ic:baseline-discord"></Icon>
+    <span>discord</span>
+  </button>
+</div>
+
+<style lang="scss">
+  @use "../styles/mixins.scss";
+
+  div {
+    position: relative;
+    display: flex;
+    flex-direction: column;
+  }
+
+  #copied {
+    position: absolute;
+    bottom: 2.5rem;
+    background-color: #23a55a;
+    padding: 0.25rem;
+    border-radius: 0.25rem;
+    align-self: center
+  }
+
+  button {
+    @include mixins.button;
+  }
+</style>
---
+++ src/components/LinkButton.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import Icon from "@iconify/svelte";
+  const { label, href, icon }: { label: string, href: string, icon?: string } = $props() // this syntax sucks
+</script>
+
+<a {href}>
+  {#if icon}
+    <Icon {icon}/>
+  {/if}
+  <span>{label}</span>
+</a>
+
+<style lang="scss">
+  @use "../styles/mixins.scss";
+  a {
+    @include mixins.button;
+  }
+</style>
