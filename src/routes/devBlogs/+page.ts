6d44e92871e65b4d741f0caed63c186c3fc191e8
mochie <187453775+mochienya@users.noreply.github.com>
2025-02-07 07:35:54 +0100

    more messing about

--- src/routes/devBlogs/+page.ts
+++ src/routes/devBlogs/+page.ts
@@ -1,15 +1,6 @@
 import { getAllBlogs } from "$lib/utils/getAllBlogs"
 
 export async function load() {
-  const blogs = (await getAllBlogs()).sort((a, b) => {
-    const date1 = a.meta.date
-    const date2 = b.meta.date
-    switch (true) {
-      case date1 < date2: return 1; break
-      case date1 === date2: return 0; break
-      case date1 > date2: return -1; break
-      default: return 0
-    }
-  })
+  const blogs = await getAllBlogs()
   return { blogs } // just let me return an array you cunt
 }
