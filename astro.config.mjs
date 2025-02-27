61b8244fc539504a28a2e8db6761163966ad8107
mochie <187453775+mochienya@users.noreply.github.com>
2025-02-27 23:50:36 +0100

    useable mobilenav and move svelte to alpine/astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,15 @@
+import alpinejs from '@astrojs/alpinejs'
 import solidJs from '@astrojs/solid-js'
-import svelte from '@astrojs/svelte'
 import icon from 'astro-icon'
 
 import { defineConfig } from 'astro/config'
 
 import UnoCSS from 'unocss/astro'
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://puppy.f53.dev',
   base: '/',
-  integrations: [UnoCSS(), solidJs(), icon(), svelte()],
+  integrations: [UnoCSS(), solidJs(), icon(), alpinejs()],
   scopedStyleStrategy: 'class',
   prefetch: {
     prefetchAll: true,
